3b7281d2047b7ce9793791093e90937be2bcf3df
Xabi Tobias <xabiertobias@gmail.com>
2017-03-27 19:20:18 +0200

    Botones de valor reseteo y check con valores negativos

--- app/src/main/java/xabi/es/zinovax/pulsaciones1/MainActivity.java
+++ app/src/main/java/xabi/es/zinovax/pulsaciones1/MainActivity.java
@@ -3,18 +3,22 @@ package xabi.es.zinovax.pulsaciones1;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     public int contador;
+    TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        textoResultado = (TextView)findViewById(R.id.contadorvalor);
         contador=0;
 
     }
@@ -23,23 +27,40 @@ public class MainActivity extends Activity {
     public void sumaContador (View vista){
         contador++;
         //Llamamos al metodo mostrarResultado;
-        mostrarResultado();
+       // mostrarResultado();
+
+        textoResultado.setText("" + contador);
     }
     //Esto es un metodo
     public void restaContador (View vista){
         contador--;
+        CheckBox negativos2 = (CheckBox)findViewById(R.id.negativos);
         //Llamamos al metodo mostrarResultado;
-        mostrarResultado();
+       // mostrarResultado();
+        if (!negativos2.isChecked()){
+            if (contador < 0 ) {
+                contador = 0;
+            }
+        }
+        textoResultado.setText("" + contador);
     }
     //Esto es un metodo
     public void reseteaContador (View vista){
-        contador=0;
+        //contador=0;
         //Llamamos al metodo mostrarResultado;
-        mostrarResultado();
+       // mostrarResultado();
+
+        EditText valorreset = (EditText)findViewById(R.id.ResetVal);
+        try {
+            contador = Integer.parseInt(valorreset.getText().toString());
+        }catch (Exception e) {
+            contador=0;
+        }
+        textoResultado.setText("" + contador);
     }
 
     //Metodo
-    public void mostrarResultado(){
+   /* public void mostrarResultado(){
         TextView valtexto = (TextView)findViewById(R.id.contadorvalor);
 
         if (contador > 0 ) {
@@ -50,9 +71,9 @@ public class MainActivity extends Activity {
             valtexto.setTextColor(Color.RED);
         }
 
-            //Metemos un blano por delante de la variable contador, si no el setText da error
+        //Metemos un blano por delante de la variable contador, si no el setText da error
         valtexto.setText("" + contador);
 
-    }
+    }*/
 
 }
